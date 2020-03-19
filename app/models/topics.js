42a8b7b775fafa8b52df2a1040bb8697f2d79c5f
wmc <1006004868@qq.com>
2020-03-19 20:47:49 +0800

    wmc

--- app/models/topics.js
+++ app/models/topics.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
 
-const topicSchema = new Schema({
-  __v: { type: Number, select: false },
-  name: { type: String, required: true },
-  avatar_url: { type: String },
-  introduction: { type: String, select: false },
-}, { timestamps: true });
+const topicSchema = new Schema(
+  {
+    __v: { type: Number, select: false },
+    name: { type: String, required: true },
+    avatar_url: { type: String },
+    introduction: { type: String }
+  },
+  { timestamps: true }
+);
 
-module.exports = model('Topic', topicSchema);
+module.exports = model("Topic", topicSchema);
