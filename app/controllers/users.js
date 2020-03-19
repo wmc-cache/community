96aca0669eb9eb56f0360ac0ba6ef7bc8c6b7386
吴梦成 <59201691+wmc-cache@users.noreply.github.com>
2020-03-19 15:49:39 +0800

    Update users.js

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -75,7 +75,7 @@ class UsersCtl {
     if (!user) { ctx.throw(401, '用户名或密码不正确'); }
     const { _id, name } = user;
     const token = jsonwebtoken.sign({ _id, name }, secret, { expiresIn: '1d' });
-    ctx.body = { token };
+    ctx.body = { token,_id };
   }
   async listFollowing(ctx) {
     const user = await User.findById(ctx.params.id).select('+following').populate('following');
