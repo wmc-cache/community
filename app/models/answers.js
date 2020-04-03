4dc36f4e095785e86bdc354f2c152112e0a66ce7
wmc <1006004868@qq.com>
2020-04-03 10:58:20 +0800

    wmc

--- app/models/answers.js
+++ app/models/answers.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
-const answerSchema = new Schema({
-  __v: { type: Number, select: false },
-  content: { type: String, required: true },
-  answerer: { type: Schema.Types.ObjectId, ref: 'User', required: true, select: false },
-  questionId: { type: String, required: true },
-  voteCount: { type: Number, required: true, default: 0 },
-}, { timestamps: true });
+const answerSchema = new Schema(
+  {
+    __v: { type: Number, select: false },
+    content: { type: String, required: true },
+    answerer: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    questionId: { type: String, required: true },
+    voteCount: { type: Number, required: true, default: 0 }
+  },
+  { timestamps: true }
+);
 
-module.exports = model('Answer', answerSchema);
+module.exports = model("Answer", answerSchema);
